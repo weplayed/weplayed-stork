d6070e91a1aa808472b6849d2d89955531a1b078
Stanyslav Yadykin <syadykin@gmail.com>
2023-12-21 20:51:24 +0200

    Add sudo to docker install

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 wp_travis_install_docker() {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  apt-get update
-  apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 wp_travis_generate_tags() {
